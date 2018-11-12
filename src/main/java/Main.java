3c10abd20c76aa4c74a9067a73f2d5b03e35a773
nErumin <nerumin90@gmail.com>
2018-11-13 03:46:23 +0900

    Add Sentry module for tracking errors.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import io.sentry.Sentry;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final String sentryDSN =
+            "https://e917f0b7f74a47cd92685dd89b0bcecd@sentry.io/1321363";
+        Sentry.init(sentryDSN);
+
+        try {
+            System.out.println("Hello, world!");
+        } catch (Exception exception) {
+            Sentry.capture(exception);
+        }
     }
 }
