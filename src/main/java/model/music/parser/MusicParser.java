dc4a7e3338d5b6340919d05af6f7a50fe0a1a53c
kjw0412c <kjw0412c@gmail.com>
2018-11-17 18:23:46 +0900

    Builder pattern adapt

--- src/main/java/model/music/parser/MusicParser.java
+++ src/main/java/model/music/parser/MusicParser.java
@@ -3,20 +3,27 @@ package model.music.parser;
 import javafx.scene.image.Image;
 import model.music.Music;
 
+import javax.sound.sampled.AudioInputStream;
+
 public abstract class MusicParser {
     protected String filePath;
     protected String title;
     protected String albumName;
     protected String artist;
     protected Image image;
+    protected AudioInputStream audioStream;
 
-    abstract MusicParser setTitle(String title);
+    abstract public MusicParser buildTitle();
 
-    abstract public MusicParser setImage(Image image);
+    abstract public MusicParser buildImage();
 
-    abstract public MusicParser setArtist(String artist);
+    abstract public MusicParser buildArtist();
 
-    abstract public MusicParser setAlbumName(String albumName);
+    abstract public MusicParser buildAlbumName();
 
-    public Music build(){ return new Music(this.title, this.artist, this.albumName, this.image); }
+    abstract public MusicParser buildAudioStream();
+
+    public Music build(){ return new Music(this.title, this.artist, this.albumName, this.image, this.audioStream); }
+
+
 }
--- src/main/java/model/music/parser/WAVParser.java
+++ src/main/java/model/music/parser/WAVParser.java
@@ -1,26 +1,55 @@
 package model.music.parser;
 
 import javafx.scene.image.Image;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.mp3.MP3File;
+import org.jaudiotagger.tag.Tag;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+import static javax.sound.sampled.AudioSystem.getAudioInputStream;
 
 public class WAVParser extends MusicParser {
+    String decoding = "ISO-8859-1";
+    String encoding = "EUC-KR";
+    File file;
+
+    public WAVParser(String filePath){
+        super.filePath = filePath;
+        file = new File(filePath);
+    }
     @Override
-    MusicParser setTitle(String title) {
-        return null;
+    public MusicParser buildTitle() {
+        return this;
     }
 
     @Override
-    public MusicParser setImage(Image image) {
-        return null;
+    public MusicParser buildImage(){
+        return this;
     }
 
     @Override
-    public MusicParser setArtist(String artist) {
-        return null;
+    public MusicParser buildArtist() {
+        return this;
     }
 
     @Override
-    public MusicParser setAlbumName(String albumName) {
-        return null;
+    public MusicParser buildAlbumName() {
+        return this;
     }
 
+    @Override
+    public MusicParser buildAudioStream() {
+
+        try {
+            super.audioStream = getAudioInputStream(file);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
 }
