2ba2ad6738aca9690259dc666ee9cdc0b4da0643
Jang Woojin <ehdwlsdnwls@naver.com>
2018-11-17 23:34:53 +0900

    make alarm gui

--- src/main/java/controller/MainGuiController.java
+++ src/main/java/controller/MainGuiController.java
@@ -2,20 +2,26 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import view.AlarmGui;
 
 public class MainGuiController {
     @FXML
     private Button favoriteMusicListBtn, fullMusicListBtn, recentPlayedMusicListBtn;
+
     public void initialize(){
 
     }
-    public void fullMusicListBtnOnClicked(){
-        fullMusicListBtn.setText("full Clicked");
-    }
+    public void fullMusicListBtnOnClicked(){ fullMusicListBtn.setText("full Clicked"); }
     public void favoriteMusicListBtnOnClicked(){
         favoriteMusicListBtn.setText("favorite Clicked");
     }
     public void recentPlayedMusicListBtnOnClicked(){
         recentPlayedMusicListBtn.setText("recent Clicked");
+    }
+
+    public void clickAlarmMenuItem(){
+        AlarmGui alarmGui = new AlarmGui();
+        alarmGui.showAndWait();
     }
 }
