6c4b0e6d30693ee3b5673c0227f20ef13c763f1e
nErumin <nerumin90@gmail.com>
2018-11-17 14:51:04 +0900

    Add basic directory readers.

--- src/main/java/io/DirectoryReader.java
+++ src/main/java/io/DirectoryReader.java
@@ -1,5 +1,19 @@
 package io;
 
-public abstract class DirectoryReader {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.NotDirectoryException;
 
+public abstract class DirectoryReader {
+    public abstract Iterable<String> getFiles(String directoryPath) throws IOException;
+
+    protected File makeFolder(String directoryPath) throws IOException {
+        final File folder = new File(directoryPath);
+
+        if (folder.isDirectory() == false) {
+            throw new NotDirectoryException("The provided path is not a directory.");
+        }
+
+        return folder;
+    }
 }
