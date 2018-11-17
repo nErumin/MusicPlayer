e52e92bc985eeb62e5cbd95535889c9b32a93885
nErumin <nerumin90@gmail.com>
2018-11-17 14:52:45 +0900

    Add directory filter with comparison to file extensions.

--- src/main/java/io/FilteredDirectoryReader.java
+++ src/main/java/io/FilteredDirectoryReader.java
@@ -1,5 +1,13 @@
 package io;
 
 public abstract class FilteredDirectoryReader extends DirectoryReader {
-    private DirectoryReader reader;
+    private final DirectoryReader decoratedReader;
+
+    public FilteredDirectoryReader(DirectoryReader decoratedReader) {
+        this.decoratedReader = decoratedReader;
+    }
+
+    protected DirectoryReader getDecoratedReader() {
+        return decoratedReader;
+    }
 }
