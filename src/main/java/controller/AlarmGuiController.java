bd00cc38adb7722b23bd3ac6f9d58b2aff7224d3
Jang Woojin <ehdwlsdnwls@naver.com>
2018-11-19 20:43:12 +0900

    make menuitems in alarm gui for select time

--- src/main/java/controller/AlarmGuiController.java
+++ src/main/java/controller/AlarmGuiController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+
 public class AlarmGuiController {
+    @FXML
+    MenuButton ampmMenuBtn, minuteMenuBtn;
+    public void initialize(){
+
+    }
+    public void alarmMenuItemClicked(){
+        ampmMenuBtn.setText("Clicked");
+    }
 }
