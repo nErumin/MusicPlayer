bac7e5e95d091914e4617ea3a2594a175bcf09b0
Jang Woojin <ehdwlsdnwls@naver.com>
2018-11-20 09:11:05 +0900

    remove unused menubutton in alarm gui controller

--- src/main/java/controller/AlarmGuiController.java
+++ src/main/java/controller/AlarmGuiController.java
@@ -1,15 +1,7 @@
 package controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.MenuButton;
-
 public class AlarmGuiController {
-    @FXML
-    MenuButton ampmMenuBtn, minuteMenuBtn;
     public void initialize(){
 
-    }
-    public void alarmMenuItemClicked(){
-        ampmMenuBtn.setText("Clicked");
     }
 }
