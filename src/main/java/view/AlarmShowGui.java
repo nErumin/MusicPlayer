b0d50748810fd843f28e3b4e7c702d01b5cbbf69
Jang Woojin <ehdwlsdnwls@naver.com>
2018-11-21 11:39:43 +0900

    make alarm show gui

--- src/main/java/view/AlarmShowGui.java
+++ src/main/java/view/AlarmShowGui.java
@@ -3,12 +3,14 @@ package view;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AlarmShowGui extends Stage {
-    public AlarmShowGui(){
+    public AlarmShowGui(String Ampm, String hour, String minute, String alarmText){
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("alarm_show_gui.fxml"));
 
         Parent root = null;
@@ -23,6 +25,12 @@ public class AlarmShowGui extends Stage {
         this.setMinHeight(230);
         this.setMaxWidth(350);
         this.setMaxHeight(230);
+        ((Label)scene.lookup("#alarmShowAmpmLabel")).setText(Ampm);
+        ((Label)scene.lookup("#alarmShowHourLabel")).setText(hour);
+        ((Label)scene.lookup("#alarmShowMinuteLabel")).setText(minute);
+        ((TextArea)scene.lookup("#alarmShowTextArea")).setText(alarmText);
         this.setTitle("Alarm Ringing");
+
+        this.setScene(scene);
     }
 }
