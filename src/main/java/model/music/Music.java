92fc731fd1ee3afaebeeefbe97e4465ea2fb0a48
nErumin <nerumin90@gmail.com>
2018-11-21 13:51:17 +0900

    Copy the audio stream when getting an audio input stream.

--- src/main/java/model/music/Music.java
+++ src/main/java/model/music/Music.java
@@ -1,35 +1,64 @@
 package model.music;
 
 import javafx.scene.image.Image;
-import model.music.parser.ParserCreator;
+import org.apache.commons.io.IOUtils;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class Music implements MusicData {
     private String title;
     private String artist;
     private String albumName;
     private Image image;
-    private AudioInputStream audioStream;
 
-    public Music(String title, String artist, String albumName, Image image, AudioInputStream audioStream){
+    private byte[] audioData;
+    private AudioFormat audioFormat;
+    private long audioSample;
+
+    public Music(String title, String artist, String albumName, Image image, AudioInputStream audioStream) {
         this.title = title;
         this.artist = artist;
         this.albumName = albumName;
         this.image = image;
-        this.audioStream = audioStream;
+
+        this.audioData = extractAudioData(audioStream);
+        this.audioFormat = audioStream.getFormat();
+        this.audioSample = audioStream.getFrameLength();
     }
-    public String getTitle(){
+
+    private byte[] extractAudioData(AudioInputStream audioInputStream) {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            IOUtils.copy(audioInputStream, outputStream);
+
+            return outputStream.toByteArray();
+        } catch (IOException exception) {
+            return null;
+        }
+    }
+
+    public String getTitle() {
         return title;
     }
-    public String getArtist(){
+
+    public String getArtist() {
         return  artist;
     }
-    public String getAlbumName(){
+
+    public String getAlbumName() {
         return albumName;
     }
-    public Image getImage(){
+
+    public Image getImage() {
         return image;
     }
-    public AudioInputStream getAudioStream() { return audioStream; }
+
+    public AudioInputStream getAudioStream() {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(audioData);
+        return new AudioInputStream(inputStream, audioFormat, audioSample);
+    }
 }
