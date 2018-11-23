454d6e2e798e7cf6d97e1ed8b27430828ca4548d
Jang Woojin <ehdwlsdnwls@naver.com>
2018-11-23 15:51:04 +0900

    make shutdown function and shutdown system

--- src/main/java/controller/ShutdownSettingGuiController.java
+++ src/main/java/controller/ShutdownSettingGuiController.java
@@ -1,8 +1,11 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import thread.ShutdownSystem;
 
 public class ShutdownSettingGuiController {
     @FXML
@@ -12,6 +15,18 @@ public class ShutdownSettingGuiController {
     }
 
     public void okBtnClicked(){
+        ComboBox timeComboBox;
+
+        Scene scene = shutdownSetBtn.getScene();
+        timeComboBox = (ComboBox)scene.lookup("#shutdownTimeComboBox");
+
+        String selectTime = (String)timeComboBox.getSelectionModel().getSelectedItem();
+        selectTime = selectTime.trim();
+
+        ShutdownSystem shutdownInstance = ShutdownSystem.getInstance();
+        shutdownInstance.setShutdown(selectTime);
+        shutdownInstance.execute();
+
         ((Stage)shutdownSetBtn.getScene().getWindow()).close();
     }
 }
